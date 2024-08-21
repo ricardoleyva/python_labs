b27c8cdc90df1147e5d918ff90e3dc0794b6830d
Ricardo Leyva <ricardomtzleyva@hotmail.com>
2024-08-20 21:59:58 -0400

    Lab3

---
+++ Lab3/mymath/geometry.py
@@ -0,0 +1,13 @@
+def area_of_rectangle(length, breadth):
+    """
+    This function returns the area of a rectangle given length and breadth
+    """
+    return length * breadth
+
+
+def area_of_circle(radius):
+    """
+    This function returns twice the area of a circle given radius
+    """
+    pi = 3.141592653589793238462643383279502884197
+    return pi * radius * radius
